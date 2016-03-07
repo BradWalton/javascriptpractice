a743f9304a3c7f4accc370e7b085ca4c471f4085
BradWalton <walton4747@gmail.com>
2016-03-06 23:24:34 -0400

    variables for clock

--- main.js
+++ main.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
 
     function displayTime() {
         var currentTime = new Date()
+        var hours = currentTime.getHours()
+        var minutes = currentTime.getMinutes()
+        var seconds = currentTime.getSeconds()
+        var clockDiv = document.getElementById()
     }
+
+
 
 });
