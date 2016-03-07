60f82ed843a162d7d0ff1a50c87fc805f15e9c9a
BradWalton <walton4747@gmail.com>
2016-03-07 00:27:40 -0400

    found and corrected error, linked jquery

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+//noinspection JSUnresolvedFunction
 /**
  * Created by Brad on 2016-03-06.
  */
 
-$(document).ready(function() {
+$(document).ready( function() {
 
     function displayTime() {
         var currentTime = new Date();
