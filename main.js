3bed00236747ed5a0aa65b1b75c643b8cf4f7254
BradWalton <walton4747@gmail.com>
2016-03-07 22:25:56 -0400

    corrected and added to function

--- main.js
+++ main.js
@@ -13,11 +13,17 @@ $(document).ready( function() {
 
         var clockDiv = document.getElementById('clock');
 
-        clockDiv.innerText = hours + ":" + minutes + ":" + seconds + "";
-
         if (seconds < 10) {
             seconds = "0" + seconds;
-        };
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+
+        clockDiv.innerText = hours + ":" + minutes + ":" + seconds + "";
     }
 
     displayTime();
