492c4739387345c15c947d0e219ca030395a39d2
BradWalton <walton4747@gmail.com>
2016-03-07 22:42:14 -0400

    finished basic clock

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ $(document).ready( function() {
         var hours = currentTime.getHours();
         var minutes = currentTime.getMinutes();
         var seconds = currentTime.getSeconds();
-
+        var meridiem = "AM";
         var clockDiv = document.getElementById('clock');
 
         if (seconds < 10) {
@@ -22,8 +22,14 @@ $(document).ready( function() {
         if (hours < 10) {
             hours = "0" + hours;
         }
-
-        clockDiv.innerText = hours + ":" + minutes + ":" + seconds + "";
+        if (hours > 12) {
+            hours = hours - 12;
+            meridiem = "PM";
+        }
+        if (hours === 0) {
+            hours = 12;
+        }
+        clockDiv.innerText = hours + ":" + minutes + ":" + seconds + "  " + meridiem;
     }
 
     displayTime();
